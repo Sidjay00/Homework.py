2dbecfce5d3995c037ea3d6464616941428bef91
K.Sidorov <2887458@gmail.com>
2023-04-16 11:38:39 +0300

    refactored

--- Lesson 1/Task_1_DaysOfWeek/main.py
+++ Lesson 1/Task_1_DaysOfWeek/main.py
@@ -1,27 +1,18 @@
 DayOfWeek = int(input('Введите число, обозначающее день недели: '))
 
-condition8 = DayOfWeek < 1
-condition9 = DayOfWeek > 7
-condition1 = DayOfWeek == 1
-condition2 = DayOfWeek == 2
-condition3 = DayOfWeek == 3
-condition4 = DayOfWeek == 4
-condition5 = DayOfWeek == 5
-condition6 = DayOfWeek == 6
-
-if (condition8 or condition9):
+if (DayOfWeek < 1 or DayOfWeek > 7):
     print(f"Вы ввели некорректный день недели. Введите цифры от 1 до 7!")
-elif condition1:
+elif DayOfWeek == 1:
     print(f"{DayOfWeek} -> Понедельник")
-elif condition2:
+elif DayOfWeek == 2:
     print(f"{DayOfWeek} -> Вторник")
-elif condition3:
+elif DayOfWeek == 3:
     print(f"{DayOfWeek} -> Среда")
-elif condition4:
+elif DayOfWeek == 4:
     print(f"{DayOfWeek} -> Четверг")
-elif condition5:
+elif DayOfWeek == 5:
     print(f"{DayOfWeek} -> Пятница")
-elif condition6:
+elif DayOfWeek == 6:
     print(f"{DayOfWeek} -> Суббота")
 else:
     print(f"{DayOfWeek} -> Воскресенье")    

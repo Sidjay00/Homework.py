74868806fe2a00fae4c3fdbfc20ea39c94ea81b5
K.Sidorov <2887458@gmail.com>
2023-04-22 15:35:10 +0300

    added solution for Task 4 Lesson 1

---
+++ Lesson 2/Task_1_from_1_to_N/main.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+N = int(input('Введите натуральное число N: '))
+int.TryParse(Console.ReadLine(), out N);
+
+Console.Write($"{N} -> ");
+for (int i = 1; i <= N; i++)
+{
+    Console.Write($"{i * i * i}, ");
+}
+

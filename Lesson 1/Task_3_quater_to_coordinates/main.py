810771f7717d818041394c7fb4e3dea08c056c09
K.Sidorov <2887458@gmail.com>
2023-04-22 17:19:17 +0300

    added a solution for task 1 Lesson 1

--- Lesson 1/Task_3_quater_to_coordinates/main.py
+++ Lesson 1/Task_3_quater_to_coordinates/main.py
@@ -3,3 +3,19 @@
 точек в этой четверти (х и у).
 1 -> x > 0, y > 0
 """
+
+try:
+    quater_number = int(input('Введите номер четверти: '))
+except ValueError:
+    print('Вы ввели не число')
+    
+if (quater_number < 1 or quater_number > 4):
+    print(f"Вы ввели некорректный номер четверти. Введите цифры от 1 до 4!")
+elif quater_number == 1:
+    print(f"{quater_number} -> x > 0, y > 0")
+elif quater_number == 2:
+    print(f"{quater_number} -> x > 0, y < 0")
+elif quater_number == 3:
+    print(f"{quater_number} -> x < 0, y < 0")
+else:
+    print(f"{quater_number} -> x < 0, y > 0")   
